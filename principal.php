57ddee2179fdc052ecab8da67bc7f1e3ca094a26
41412257 <41412257@090025-P33S13.macklabs.br>
2015-03-26 11:56:24 -0300

    Conexão no banco.

--- principal.php
+++ principal.php
@@ -7,7 +7,16 @@
     </head>
     <body>
         <?php
-        echo "Hello Word";
+        $nome = "Ezequiel";
+        $url = "https://appear.in/psi-kasjdklsdjaskl";
+        echo "Hello Word Sua url para consulta com". $nome. " é <a href=\"". $url."\">SESSAO</a>"  ;
         ?>
     </body>
+    
+    <!--
+    
+    O usuario se cadastra tem a opção de escolher os psicologos que terão seus curriculs a disposição
+    o psicologo tem sua agenda com os horarios disponiveis
+    
+    -->
 </html>
