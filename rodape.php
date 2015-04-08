156e61fe1134261d8ff526771208a349ca3727bf
Brendoon Ryos <brendoon-ryos@live.com>
2015-04-07 23:22:44 -0300

    Remoção de código desnecessário.

--- rodape.php
+++ rodape.php
@@ -1,15 +1,3 @@
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" type="text/css" href="style.css" />
-        <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-        <title></title>
-    </head>
-    <body>
         <footer>
            
         </footer>
-    </body>
-</html>
