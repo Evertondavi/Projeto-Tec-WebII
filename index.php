05a91c86a3f83ec375b084d20464865fa3b44cda
Brendoon Ryos <brendoon-ryos@live.com>
2015-04-07 23:32:11 -0300

    Identação.

--- index.php
+++ index.php
@@ -1,35 +1,26 @@
-<?php
-  // conctando ao BD  include "conecta_mysql.php";
-  
-?>
 <!DOCTYPE html>
 
 <html>
-<head>
-<meta charset="UTF-8">
-<title></title>
-    <style>
-     form{width:300px;margin:0 auto;margin-top:200px;}
-     #botao{background:red;margin-left:90px;}
-     
-     
-     
-    </style>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            form{width:300px;margin:0 auto;margin-top:200px;}
+            #botao{background:red;margin-left:90px;} 
+        </style>
 
 
-</head>
-<body>
-    <form action="create.php" method="POST">
+    </head>
+    <body>
+        <form action="create.php" method="POST">
         
-        <p>Name
-        <input type="text" name="nome" />
-        </p>
-        <p>Senha
-        <input type="password"  pattern="[0-9]*" name="senha" />
-        </p>
-        <input type="submit" value="Cadastrar" id="botao" />
-        
-        
-    </form>
-</body>
+            <p>Name
+                <input type="text" name="nome" />
+            </p>
+            <p>Senha
+                <input type="password"  pattern="[0-9]*" name="senha" />
+            </p>
+                <input type="submit" value="Cadastrar" id="botao" />
+        </form>
+    </body>
 </html>
