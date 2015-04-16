da3db853c829b9d434841fa4671a8847e1556115
leosiqueira12 <leonardo_siqueira12@hotmail.com>
2015-04-16 11:20:44 -0300

    Update cadastro.php

--- users/cadastro.php
+++ users/cadastro.php
@@ -1,84 +1,109 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Cadastro de Usuáiros</title>
-<script>
-		function valida(form){
-		if(form.nome.value==""){
-		alert("Preencha o nome corretamente:");
-		form.nome.focus();
-		returne false;
-		}
-		function valida(form){
-		if(form.sobrenome.value==""){
-		alert("Preencha o sobrenome corretamente:");
-		form.nome.focus();
-		returne false;
-		}
-		var filtro_mail = /^.+@.+\..{2,3}$/
-        if(!filtro_mail.test(form.email.value) || form.email.value=="") {
-        alert("Preencha o E-mail corretamente.");
-        form.email.focus();
-        return false;
-        }
-		if (form.senha.value=="" || form.senha.value.length < 10) {
-        alert("Preencha a senha corretamente.");
-        form.senha.focus();
-        return false;
-        }
-        if (form.conf_senha.value=="" || form.conf_senha.value.length < 10) {
-        alert("Preencha a confirma??o de senha corretamente.");
-        form.conf_senha.focus();
-        return false;
-        }
-        if (form.senha.value!=form.conf_senha.value) {
-        alert("A senha e a confirma??o tem de ser iguais.");
-        form.conf_senha.focus();
-        return false;
-		}
-		if (form.sexo[0].checked==false && form.sexo[1].checked==false) {
-        alert("Selecione o sexo.");
-        return false;
-        }
-		var Soma; 
-		var Resto; 
-		Soma = 0; 
-		if (strCPF == "00000000000") return false; 
-		for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
-		Resto = (Soma * 10) % 11;
-		if ((Resto == 10) || (Resto == 11)) Resto = 0;
-		if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
-		Soma = 0;
-		for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
-		Resto = (Soma * 10) % 11;
-		if ((Resto == 10) || (Resto == 11)) Resto = 0; if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
-		return true;
-		} 
-		var strCPF = "12345678909";
-		alert(TestaCPF(strCPF));
+ <meta charset="UTF-8">
+ <title>login</title>
+ <script type="text/javascript">
+  function checkValue() {
+
+  var username = document.frm.nome.value;
+  var sobre = document.frm.sobrenome.value;
+  var senha = document.frm.senha.value;
+  var confirmar = document.frm.confirmarSenha.value;
+  var useremail = document.frm.email.value;
+
+  console.log(username);
+  console.log(senha);
+  console.log(confirmar);
+  console.log(useremail);
+  console.log(sobre);
+
+  //유효성검사
+  if(username == '') {
+   alert("Preencha o nome corretamente.");
+   document.frm.nome.focus();
+   return; // 반환후 중지
+  }
+  if(sobre == '') {
+   alert("Preencha o sobrenome corretamente.");
+   document.frm.sobrenome.focus();
+   return; // 반환후 중지
+  }
+  if(senha == ''){
+   alert("Preencha a senha corretamente.");
+   document.frm.senha.focus();
+   return;
+  }
+  if(confirmar == ''){
+   alert("Preencha confirmar senha.");
+   document.frm.confirmarSenha.focus();
+   return;
+  }
+  if(useremail == ''){
+   alert("Preencha email corretamente.");
+   document.frm.email.focus();
+   return;
+  }
+  // 비밀번호 일치 여부
+  if(senha != confirmar){
+   alert("A senha nao é igual.");
+   document.frm.senha.value = '';
+   document.frm.confirmarSenha.value = '';
+   document.frm.senha.focus();
+   return;
+  }
+  // 이메일 형식
+  if(useremail.indexOf('@') < 1)
+  {
+   alert("Preencha.");
+   document.frm.email.value = '';
+   document.frm.email.focus();
+   return;
+  }
+  // 올바른 양식이 입력된 것을 확인했다.
+  // 현재 양식을 다른 페이지로 제출(값 전달)
+  
+  document.frm.action="test.html";
+  document.frm.method="get";
+  document.frm.submit();
+ }
 </script>
 </head>
-
 <body>
-
-<h1> Cadastro de Usuáiros </h1>
-
-<form action="create.php" method="Post" name="cadastroU">
-
-<h3>Nome:</h3>
-<input type="text" name="nome" placeholder="Nome"/>
-<h3>Sobrenome:</h3>
-<input type="text" name="sobrenome" placeholder="Sobrenome"/>
-<h3>E-mail:</h3>
-<input type="email" name="email" placeholder="E-mail"/>
-<h3>Senha:</h3>
-<input type="password" name="senha" size="10" maxlength="10" placeholder="Senha"/>
-<br></br>
-
-<input type="submit" value="Cadastrar" name="cadastrar">
-</form>
-
+ <form name="frm" method="get" action="test.html">
+  <table>
+   <tr>
+    <th>Nome:</th>
+    <td><input type="text" name="nome" id="" placeholder ="Nome"></td>
+   </tr>
+   <tr>
+    <th>Sobrenome:</th>
+    <td><input type="text" name="sobrenome" id="" placeholder="Sobrenome"></td>
+   </tr>
+   <tr>
+   <tr>
+    <th>E-mail:</th>
+    <td><input type="text" name="email" id="" placeholder="E-mail"></td>
+   </tr>
+   <tr>
+   <tr>
+    <th>Senha:</th>
+    <td><input type="password" name="senha" id="" placeholder="Senha"></td>
+   </tr>
+   <tr>
+    <th>Confirmar senha:</th>
+    <td><input type="password" name="confirmarSenha" id="" placeholder="Confirmar senha"></td>
+   </tr>
+   <th>Sexo:</th>
+                <td>
+                    <input type="radio" name="sexo" value="masculino" checked="checked">Masculino
+                    <input type="radio" name="sexo" value="feminino">Feminino
+                </td>
+   </tr>
+    <td colspan="2"><input type="button" value="Registrar" onclick="checkValue()"></td>
+   </tr>
+  </table>
+ </form>
 </body>
 </html>
 
