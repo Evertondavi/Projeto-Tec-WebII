a996acc1ea6c7b7878c0dc0f5f7f37542c37dc46
Everton Davi <evertondavi1990@hotmail.com>
2015-05-06 10:16:29 -0300

    Pasta de usuarios

---
+++ users/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            form{width:300px;margin:0 auto;margin-top:200px;}
+            #botao{background:red;margin-left:90px;} 
+        </style>
+
+
+    </head>
+    <body>
+        <form action="create.php" method="POST">
+        
+            <p>Name
+                <input type="text" name="nome" />
+            </p>
+            <p>Senha
+                <input type="password"  pattern="[0-9]*" name="senha" />
+            </p>
+                <input type="submit" value="Cadastrar" id="botao" />
+        </form>
+    </body>
+</html>
