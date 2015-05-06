686ad1e50597593105bbcc6838242bc72385c32a
Everton Davi <evertondavi1990@hotmail.com>
2015-05-06 10:16:11 -0300

    Pagina de comentario

--- conecta_mysql.php
+++ conecta_mysql.php
@@ -3,33 +3,13 @@
 
   // abrir conexao e selecionar o banco de dados
   // CONFERIR A PORTA ABERTA PARA MYSQL NO MACKENZIE - 3308
-  $conn = mysqli_connect("localhost","root","administrador");
-  $sql = "CREATE DATABASE  IF NOT EXISTS myDB";
-  if (mysqli_query($conn, $sql)) {
-    //echo "Database created successfully";
-  } else {
-    echo "Error creating database: " . mysqli_error($conn);
-  }
-mysqli_close($conn);
-    $conexao = mysqli_connect("localhost","root","administrador", "myDB");
- 
-  
+   $banco = "login";
+   $conexao = mysql_connect("localhost","root","");
+   mysql_select_db($banco) or die(mysql_error());
   
   // tratamento de erros
-  if (mysqli_connect_errno())
-  {
-      echo "Não foi possível conectar: " . mysqli_connect_error();
-  } else {
-  
-  $myfile = fopen("banco.sql", "r") or die("Unable to open file!");
-  $sql = fread($myfile,filesize("banco.sql"));
-  mysql_connect ("localhost","root","administrador");
-  mysql_select_db('myDB') or die('Could not select database.');
-  //"[ string $server [", string $username [, string $password [, bool $new_link [, int $client_flags ]]]]] )
-  mysql_query($sql);
-	
-	
-}  
+  	
+
 ?>
 
 
