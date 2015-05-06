832d2cb200d7d2f78d4201b0bee975f400c7f11f
Everton Davi <evertondavi1990@hotmail.com>
2015-05-06 10:00:08 -0300

    Pagina dop  administrador para verificar comentario

--- admin/noticias.php
+++ admin/noticias.php
@@ -14,13 +14,14 @@
      
 	$seleciona = mysql_query("SELECT * FROM comentario WHERE status ='nao'");
 		echo $seleciona;
-               if($seleciona < 1){
+               if($seleciona == null){
 			echo "Você não tem comentarios";
 		}else{
 			
 	
 		while($linha = mysql_fetch_array($seleciona)){
 			 echo "<h3>" .$linha['nome']."</h3><br/>";
+                         echo "<p>".$linha['email']."</p><br/>";
 			 echo "<p>".$linha['comentario']."</p><br/>";
 			 echo "<a href=\"permitir.php?id= " . $linha['id']. "\">Permitir</a>";
 			 echo "<span>        </span>";
