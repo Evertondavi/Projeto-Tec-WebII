8dbe2514e4bdfce32dabb539af9269b5684573ea
Everton Davi <evertondavi1990@hotmail.com>
2015-05-06 09:51:33 -0300

    Pagina para inserir noticias.

--- admin/insert.php
+++ admin/insert.php
@@ -13,23 +13,40 @@ and open the template in the editor.
     <body>
         <form action="#"  enctype="multipart/form-data" method="post">
             <p>Titulo:</p><input type="text" name="titulo" maxlength="100"/>
-			<p>Imagem:</p><input type="text" name="imagem"/>
+			<p>Imagem:</p><input type="file" name="img"/>
 			<p>Texto:</p><textarea type="text" name="texto" cols="30" rows="10"></textarea><br />
 			<input type="hidden" name="acao" value="cad" />
 			<input type="submit"value="cadastrar" />
         </form>
 <?php 
 	if(isset($_POST['acao']) && $_POST['acao'] == 'cad'){
-	  $titulo = $_POST['titulo'];
-	  $imagem = $_POST['imagem'];
+	 $pasta = 'img'; 
+	 $permitido = array('image/jpg', 'image/jpeg', 'image/pjpeg' );
+	
+	$img = $_FILES['img'];
+	$tmp = $img['tmp_name'];
+        $name = $img['name'];
+	$type = $img['type'];
+	
+	require ('funcao.php');
+	
+	if(!empty($name) && in_array($type, $permitido)){
+		$nome = 'downsmaster-'.md5(uniqid(rand(), true)).'.jpg';
+		upload($tmp, $nome, 500, $pasta);
+          
+
+
+          $titulo = $_POST['titulo'];
+	 // $foto = $_POST['foto'];
 	  $texto = $_POST['texto'];
 	  
 	  if(empty($titulo) || empty($texto)){
 		echo '<script>alert("Preencha todos os campos");</script>';
 	  }else{
-	    $inserir = mysql_query("INSERT INTO postagem (titulo, imagem , texto)VALUES('$titulo', '$imagem', '$texto')");
+	    $inserir = mysql_query("INSERT INTO image (titulo, foto , texto)VALUES('$titulo', '$nome', '$texto')");
 		echo '<script>alert("Postagem cadastrada com sucesso!!");</script>';
 	  }
+         }
 	}
 ?>
     </body>
