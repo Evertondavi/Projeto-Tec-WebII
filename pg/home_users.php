edd94bf02ac7dc534b6c7b77213f09adc897c9ab
leosiqueira12 <leonardo_siqueira12@hotmail.com>
2015-05-07 11:32:12 -0300

    Body do users

--- pg/home_users.php
+++ pg/home_users.php
@@ -7,3 +7,20 @@
         <link rel="icon" href="img\logo.ico" type="image/x-icon" />
         <title>Psicologo</title>
     </head>
+ <body>
+        <header class="topo">
+	
+            
+            <img src="img\logo.png" alt="logo" class="imgTopo" />
+            <nav class="menu">
+                <ul class="a">
+                    <li><a href="home_users.php">Home</a></li>
+                    <li><a href="home_users.php?pg=perfil">Perfil</a></li>
+                    <li><a href="home_users.php?pg=mensagens">Mensagens</a></li>
+                    <li><a href="home_users.php?pg=curiosidades">Curiosidades</a></li>
+                    <li><a href="home_users.php?pg=livros">Livros</a></li>
+					<li><a href="home_users.php?pg=consultas">Marcar Consultas</a></li>
+                </ul>
+            </nav>
+	 
+	 </header>    
