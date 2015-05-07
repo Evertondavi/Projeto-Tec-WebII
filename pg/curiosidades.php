cba368e615c6e9c603bc81d53ec09a6a47f3c0a3
leosiqueira12 <leonardo_siqueira12@hotmail.com>
2015-05-07 11:40:38 -0300

    Inicio de curiosidades

--- pg/curiosidades.php
+++ pg/curiosidades.php
@@ -1,1 +1,60 @@
 
+<article class="noticias">
+		<p class="noticiaP1">Resumo
+		</p>
+		<p class="noticiaP2">
+			A evasão no ensino superior é um fenômeno que tem chamado a atenção de gestores e pesquisadores nas últimas décadas. Observa-se na literatura internacional a tentativa de elaboração de diversos modelos teóricos sobre evasão e persistência no ensino superior. Essas pesquisas internacionais, de um modo geral, 
+			são elaboradas por pesquisadores que têm como foco o estudo e a avaliação do ensino superior. No Brasil, as pesquisas em evasão são realizadas por pesquisadores dentro de suas próprias áreas. Elas tendem a ser descritivas e exploratórias, focadas no diagnóstico específico de um curso ou instituição. Geralmente, não há um aprofundamento teórico e quando há tende-se a utilizar a teoria de Vincent Tinto como referencial. Contudo, estudos nacionais e internacionais têm sugerido que essa teoria é insuficiente para dar conta do fenômeno. Dado esse quadro, este artigo tem o objetivo de apresentar outros modelos de evasão universitária e discutir os rumos das pesquisas sobre evasão no Brasil. Conclui-se que, para melhorar a qualidade das pesquisas realizadas no Brasil é necessária a elaboração de um modelo de evasão aplicável ao contexto nacional.
+
+        </p>
+		<a>+ Saiba mais</a>
+</article>
+
+
+<article class="noticias">
+		<p class="noticiaP1">Violência no Namoro Durante a Adolescência Pode Afetar Educação e Situação Econômica no Futuro
+		</p>
+		<p class="noticiaP2">
+			Além do pedágio óbvio na saúde física e emocional, violência em namoro na adolescência tende a levar a menor educação e rendimentos mais baixos mais tarde na vida, de acordo com um pesquisador da Universidade Estadual de Michigan. 
+        </p>
+		<a>+ Saiba mais</a>
+</article>
+
+
+<article class="noticias">
+		<p class="noticiaP1">O Que Aciona a Criatividade?
+		</p>
+		<p class="noticiaP2">
+		Entender a criatividade tem sido uma meta de filósofos e cientistas de vários milênios.
+
+        </p>
+		<a>+ Saiba mais</a>
+		
+</article>
+	
+	
+	<!-------DEPOIMENTOS---------->
+	
+ <article class="Depoimentos">
+		<p class="depoimentoP1">“Ético, sensível, carismático, Felipe é uma dessas raras pessoas em quem se pode contar e confiar sempre. Profissional atento e dedicado, vem desenvolvendo um trabalho super abrangente na internet, colocando a disposição de todos centenas de textos voltados para o aperfeiçoamento pessoal e autoconhecimento, além de manter-se disponível para orientações online. Seu caráter é indiscutível e seu talento interpessoal contagiante. Uma pessoa a quem só posso elogiar”.      
+			Kattlyn Pereira, Paciente
+
+		</p>
+		
+	
+</article>
+<article class="Depoimentos">
+		<p class="depoimentoP1">“Um ser humano notável, que se dedica a mostrar que todos nós também somos seres notáveis. Um excelente profissional cujo o objetivo é simplesmente ajudar. Tem um trabalho fantástico na sua página compartilhando conhecimentos, dicas e dividindo também suas vivências como profissional e pessoal. É uma pessoa que vale apena conhecer, ser conhecido por ele e assim se conhecer melhor!”
+			Markinhos Souza, Paciente
+		</p>
+		
+		
+</article>
+<article class="Depoimentos">
+		<p class="depoimentoP1">“Uma pessoa doce, um grande ouvinte. Felipe de Souza fala manso e sempre tem uma palavra sensata para nos ajudar. Como sou uma pessoa muito ansiosa, o fato de ele ser tranquilo, me transmitia muita paz. Sucesso em seu site. Tenho certeza que os internautas terão nesse espaço boas oportunidades de crescimento e conhecimento pessoal”.
+			Magda Velloso, Paciente
+
+		</p>
+		
+		
+</article>
