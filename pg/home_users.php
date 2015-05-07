9ace0554d756e2b27f4edca3920f5f7b5d0aedcb
leosiqueira12 <leonardo_siqueira12@hotmail.com>
2015-05-07 11:31:44 -0300

    header do php

--- pg/home_users.php
+++ pg/home_users.php
@@ -1,1 +1,9 @@
-
+<!DOCTYPE html>
+<html>
+    <head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="css\users-index.css" type="text/css" />
+	<link rel="stylesheet" href="css\pgStyle.css" type="text/css" />
+        <link rel="icon" href="img\logo.ico" type="image/x-icon" />
+        <title>Psicologo</title>
+    </head>
