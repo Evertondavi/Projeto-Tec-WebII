137ed69f940387a7c8abac60a6caf8583972e485
CristinaJuhee <juhee940@gmail.com>
2015-05-07 10:59:47 -0300

    Update login.php

--- users/login.php
+++ users/login.php
@@ -26,3 +26,59 @@ if($res == false){
    mysqli_close($conexao);
 ?>
 
+<!--
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Log in</title>
+<script type="text/javascript">
+function checkValue() {
+
+var useremail = document.frm.email.value;
+var senha = document.frm.senha.value;
+
+ console.log(useremail);
+  console.log(senha);
+  
+   if(useremail == ''){
+   alert("Preencha email corretamente.");
+   document.frm.email.focus();
+   return;
+  }
+  
+  if(senha == ''){
+   alert("Preencha a senha corretamente.");
+   document.frm.senha.focus();
+   return;
+  }
+  
+   document.frm.action="Login.html";
+  document.frm.method="get";
+  document.frm.submit();
+ }
+ 
+ </script>
+</head>
+<body>
+<form name="frm" method="get" action="Login.html">
+ <table>
+  <tr>
+    <th>E-mail</th>
+    <td><input type="text" name="email" id="" placeholder="E-mail"></td>
+   </tr>
+   <tr>
+   <tr>
+    <th>Senha</th>
+    <td><input type="password" name="senha" id="" placeholder="Senha"></td>
+   </tr>
+   <td colspan="2"><input type="button" value="Registrar" onclick="checkValue()"></td>
+   </tr>
+   <tr>
+   <td><a href="cadastro.html">Cadastra-se</a></td>
+   </tr>
+  </table>
+ </form>
+</body>
+</html>
+-->
