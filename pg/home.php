6a045dff66005724ee2badf9242d8d97ae66de83
leosiqueira12 <leonardo_siqueira12@hotmail.com>
2015-05-20 10:13:25 -0300

    Inserindo like button na home

--- pg/home.php
+++ pg/home.php
@@ -9,6 +9,14 @@
     </head>
     
     <body>
+        <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
         <header class="topo">
 	 
             <form action="create.php" method="POST" class="form">
@@ -44,7 +52,7 @@
         ?>
         <footer class="rodape">
             <p>© Copyright - Todos os Direitos Reservados</p>
+            <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
         </footer>
     </body>
 </html>
-
