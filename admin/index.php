474c194444a354c552c78d04070acc5c17e416ab
Evertondavi <evertondavi1990@hotmail.com>
2015-05-19 22:44:54 -0300

    Update index.php
    Login admin pronto

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,10 @@
   // conctando ao BD  include "conecta_mysql.php";
   
 ?>
+<?php
+  // conctando ao BD  include "conecta_mysql.php";
+  
+?>
 <!DOCTYPE html>
 
 <html>
@@ -9,18 +13,25 @@
 <meta charset="UTF-8">
 <title></title>
     <style>
-     form{width:300px;margin:0 auto;margin-top:200px;}
-     #botao{background:red;margin-left:90px;}
-     
-     
+     form{ 
+ 	border:1px solid #000;height:300px; border-radius:8px;background: #2D3688;
+     width:500px;margin:0 auto;margin-top:150px;}
+     #botao{background:#fff;margin-left:55px;width:100px;color:#2D3688;}
+
+     form .login{margin:0 auto;width:200px;margin-top:100px;color:#fff;}
+     .imgL{margin: 0 auto; margin-top: 150px;border:1px solid #FFF;width:90px;}
+     .imgL img{float: left;}
      
     </style>
 
 
 </head>
 <body>
+<article class="imgL">
+<img src="logo.png" />
+</article>
     <form action="login.php" method="POST">
-        
+        <article class="login">
         <p>Name
         <input type="text" name="nome" />
         </p>
@@ -28,7 +39,7 @@
         <input type="password"  name="senha" />
         </p>
         <input type="submit" value="Logar" id="botao" />
-        
+        </article>
         
     </form>
 </body>
