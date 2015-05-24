3c248b048e71dcbf28a7f3ce71a4f2d4b7c26a30
Brendoon <brendoon-ryos@live.com>
2015-05-24 19:18:39 -0300

    Update insertCom.php

--- admin/insertCom.php
+++ admin/insertCom.php
@@ -3,21 +3,35 @@
 	
    
 	if (isset($_POST['acao'])&& $_POST['acao']== 'enviar'){
-		$nome = trim(strip_tags(ucwords($_POST['nome'])));
+		$n = trim(strip_tags(ucwords($_POST['nome'])));
 		$email = trim(strip_tags($_POST['email']));
 		$comentario = trim(strip_tags($_POST['coment']));
 		
-	if(empty($nome) || empty($email) || empty($comentario)){
+	if(empty($n) || empty($email) || empty($comentario)){
 	  echo '<script>alert("Preencha todos os campos");</script>';
-	}elseif(!preg_match("/^[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\.\-]+\.[a-z]{2,4}$/i",$email))
+	}elseif(!preg_match("/^[a-z0-9_\.\-]+@[a-z0-9_\.\-]*[a-z0-9_\.\-]+\.[a-z]{2,4}$/i",$email)){
 	  echo '<script>alert("E-mail Inválido");</script>';
-	else{
+	}else{
 	  $status = 'nao';
-	  $inserir = mysql_query("INSERT INTO comentario (nome, email, comentario, status)VALUES('$nome','$email', '$comentario' ,'$status') ");
-	  echo '<script>alert("Mensagem enviada com sucesso!!!");</script>';
-	  header('Location: ../comentarios.php');
-	}
-	}
+	 $pasta = '../img'; 
+	 $permitido = array('image/jpg', 'image/jpeg', 'image/pjpeg' );
 	
+	$img = $_FILES['img'];
+	$tmp = $img['tmp_name'];
+        $name = $img['name'];
+	$type = $img['type'];
+	
+	require ('funcao.php');
+	if(!empty($name) && in_array($type, $permitido)){
+		$nome = 'downsmaster-'.md5(uniqid(rand(), true)).'.jpg';
+		upload($tmp, $nome, 200, $pasta);
+
+
+	  $inserir = mysqli_query($conexao,"INSERT INTO comentario (nome, email, comentario, image,status)VALUES('$n','$email', '$comentario' ,'$nome','$status') ");
+	  echo '<script>alert("Mensagem enviada com sucesso!!!");</script>';
+	  header('Location: ../pg/home.php');
+	}
+	}
+}
+	 mysqli_close($conexao);
 	 ?>
-  
