bc101872a888feb2e061ccc5a66f71dfaa12cdfd
leosiqueira12 <leonardo_siqueira12@hotmail.com>
2015-05-24 15:33:18 -0300

    Update excluirN.php

--- admin/excluirN.php
+++ admin/excluirN.php
@@ -1,1 +1,11 @@
 
+<?php include "conecta_mysql.php";
+
+		  
+		  mysqli_query ($conexao,"DELETE FROM image WHERE id = $_GET[id]")or die (mysql_error());
+		  header('Location: faq.php?admin=conteudo');
+		     
+	
+	
+		
+	?>
