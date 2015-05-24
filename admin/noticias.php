19f0e68647bf7c4d4c14eb85fbcf3279c16465bc
Brendoon <brendoon-ryos@live.com>
2015-05-24 19:21:21 -0300

    Update noticias.php

--- admin/noticias.php
+++ admin/noticias.php
@@ -1,9 +1,8 @@
-<?php include "conecta_mysql.php";?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link  href="estilo.css" rel="stylesheet" type="text/css" />
+        <link  href="..\css\cssFE.css" rel="stylesheet" type="text/css" />
         
         
     </head>
@@ -11,36 +10,34 @@
 	
 	 
 <?php 
-     
-	$seleciona = mysql_query("SELECT * FROM comentario WHERE status ='nao'");
-		echo $seleciona;
-               if($seleciona == null){
+     include "conecta_mysql.php";
+	$seleciona = mysqli_query($conexao,"SELECT * FROM comentario WHERE status ='nao'");
+	
+               if(mysqli_num_rows($seleciona) == null){
 			echo "Você não tem comentarios";
 		}else{
 			
-	
-		while($linha = mysql_fetch_array($seleciona)){
-			 echo "<h3>" .$linha['nome']."</h3><br/>";
-                         echo "<p>".$linha['email']."</p><br/>";
-			 echo "<p>".$linha['comentario']."</p><br/>";
+		echo "<section class='allC'>";
+		while($linha = mysqli_fetch_array($seleciona)){
+			echo  "<article class='tudoCOM'>";
+			
+			$imagem   = $linha['image'];
+			  echo  "<img  class='imgCa' src='../img/$imagem'>";
+			 echo "<h3 class='name'>" .$linha['nome']."</h3><br/>";
+            		echo "<p class='email'>".$linha['email']."</p><br/>";
+			 echo "<p class='text'>".$linha['comentario']."</p><br/>";
 			 echo "<a href=\"permitir.php?id= " . $linha['id']. "\">Permitir</a>";
 			 echo "<span>        </span>";
 			 echo "<a href=\"excluir.php?id= " . $linha['id']. "\">Deletar</a>";
+			 echo "</article>";
+			 } 
+			 echo "</section>";
+			 }
+
+			
+                mysqli_close($conexao);
 ?>
 
 
- 
-<?php 
-	} }
-
-			
-		
-               
-			
-            
-			 
-			 
-			 
-?>
 </body>
 </html>
