76500fb052fd35e9d63763462e2d8d60a95600f0
leosiqueira12 <leonardo_siqueira12@hotmail.com>
2015-05-24 15:31:21 -0300

    Update conteudo.php

--- admin/conteudo.php
+++ admin/conteudo.php
@@ -1,36 +1,48 @@
-<?php include "conecta_mysql.php";?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link  href="estilo.css" rel="stylesheet" type="text/css" />
-        
+          <link rel="stylesheet" href="..\css\cssAd.css" type="text/css" />
         
     </head>
     <body>
-        
-     <?php
-		$seleciona = mysql_query("SELECT * FROM image ORDER BY id DESC") or die (mysql_error());;
-		$conta = mysql_num_rows($seleciona);
+       <section >
+    <?php
+	 include "conecta_mysql.php";
+		$seleciona = mysqli_query($conexao,"SELECT * FROM image ORDER BY id") or die (mysql_error());;
+		$conta = mysqli_num_rows($seleciona);
 		
 		if($conta <= 0){
 			echo "Não ha nenhum dado no banco";
 		}else{
-		   while($ln = mysql_fetch_array($seleciona)){
+					    echo	"<section class='tudoTO'>";
+		   while($ln = mysqli_fetch_array($seleciona)){
+
+		   echo  "<article class='tudoC'>";
+                   $pega = $ln['id']; 
+		   $id = "<a class='btED' href=\"form_edit_img.php?&id= " . $pega. "\">Editar</a>";	
+		   $ex = "<a class='btEx' href=\"excluirN.php?&id= " . $pega. "\">Excluir</a>";	
 		   $titulo   = $ln['titulo'];
 		   $imagem   = $ln['foto'];
 		   $conteudo = $ln['texto'];
-		   
-		
+		 
+		  echo "<h1 class='txtH'>$titulo<h1/>";
+		  echo  "<img class='imgI' src='../img/$imagem'>";
+		  echo "<p class='txtC'>$conteudo</p>";
+		 
+		  echo   $id;	
+	          echo  $ex;	
+		  echo "</article>";
+			
+	          	
+}
+ 	
+echo "</section>";
+
+}
+
 	 ?>
-        <article id="post">
-            <h1 ><?php echo $titulo;?></h1>
-            <img src="img/<?php echo $imagem; ?>" />
-            <p><?php echo $conteudo;?></p>
-           
-        </article>
-
-<?php }}    echo $imagem?>
-
+</section>	
 </body>
 </html>
