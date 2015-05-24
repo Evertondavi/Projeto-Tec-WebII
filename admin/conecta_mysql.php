92d59d635a7f0ed6d044b0cada4b94a03963ced5
leosiqueira12 <leonardo_siqueira12@hotmail.com>
2015-05-24 14:42:18 -0300

    Update conecta_mysql.php

--- admin/conecta_mysql.php
+++ admin/conecta_mysql.php
@@ -7,6 +7,17 @@
    mysql_select_db($banco) or die(mysql_error());
   
   // tratamento de erros
+     	$host = "mysql.hostinger.com.br";
+$user = "u548445490_root";
+$pass = "123456";
+$banco = "u548445490_psico";
+//$banco = "login";
+  $conexao = mysqli_connect($host,$user,$pass)or trigger_error(mysql_error(),E_USER_ERROR);
+
+//seleciona banco de dados
+
+ mysqli_select_db($conexao,$banco);
+ //mysqli_close($conexao);
   	
 	
 
